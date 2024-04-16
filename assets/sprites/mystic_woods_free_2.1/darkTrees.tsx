9c1a389a05046b08e1844d266198d9b000d9499c
sagaw <eta19777@gmail.com>
2024-04-16 18:30:18 +0300

    ShapeDrawer

---
+++ assets/sprites/mystic_woods_free_2.1/darkTrees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="darkTrees" tilewidth="16" tileheight="16" tilecount="144" columns="24">
+ <image source="nature free/96x96 tress.png" width="384" height="96"/>
+</tileset>
