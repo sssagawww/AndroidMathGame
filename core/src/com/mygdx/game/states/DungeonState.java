ef0adf33a54bb5494ef5811cdef01347ffacb790
mearlixxx <andreymeskin@gmail.com>
2024-04-23 15:43:32 +0300

    q

---
+++ core/src/com/mygdx/game/states/DungeonState.java
@@ -0,0 +1,378 @@
+package com.mygdx.game.states;
+
+import static com.mygdx.game.MyGdxGame.V_HEIGHT;
+import static com.mygdx.game.MyGdxGame.V_WIDTH;
+import static com.mygdx.game.handlers.B2DVars.BIT_PENEK;
+import static com.mygdx.game.handlers.B2DVars.BIT_PLAYER;
+import static com.mygdx.game.handlers.B2DVars.BIT_TROPA;
+import static com.mygdx.game.handlers.B2DVars.PPM;
+import static com.mygdx.game.handlers.GameStateManager.BATTLE;
+import static com.mygdx.game.handlers.GameStateManager.FOREST;
+import static com.mygdx.game.handlers.GameStateManager.MAZE;
+import static com.mygdx.game.handlers.GameStateManager.MENU;
+import static com.mygdx.game.handlers.GameStateManager.PAINT;
+
+import static jdk.jfr.internal.consumer.EventLog.stop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.AddAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.Dialog.Dialog;
+import com.mygdx.game.Dialog.DialogController;
+import com.mygdx.game.Dialog.DialogNode;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.UI.Controller;
+import com.mygdx.game.UI.DialogBox;
+import com.mygdx.game.UI.JoyStick;
+import com.mygdx.game.entities.B2DSprite;
+import com.mygdx.game.entities.PlayEntities;
+import com.mygdx.game.entities.Player2;
+import com.mygdx.game.handlers.BoundedCamera;
+import com.mygdx.game.handlers.Controllable;
+import com.mygdx.game.handlers.GameStateManager;
+import com.mygdx.game.handlers.MyContactListener;
+
+public class DungeonState extends GameState implements Controllable {
+
+    private Player2 player;
+    private float tileMapHeight;
+    private float tileSize;
+    private float tileMapWidth;
+    private Skin skin_this;
+    private BoundedCamera b2dCam;
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private InputMultiplexer multiplexer;
+    private MyContactListener cl;
+    private Music music;
+    private PlayEntities entities;
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer tmr;
+    private BodyDef bdef;
+    private JoyStick joyStick;
+    private BoundedCamera joyCam;
+    private ShapeRenderer shapeRenderer;
+    private Vector3 mouse;
+    private Controller controller;
+    private Stage controllerStage;
+    private boolean isStopped = false;
+    private boolean canDraw = false;
+    private Stage uiStage;
+    private DialogController dcontroller;
+    private float time = 0;
+    private DialogBox dialogueBox;
+    private boolean debug = false;
+    private Dialog dialog;
+    private int nextState;
+
+    public DungeonState(GameStateManager gsm) {
+        super(gsm);
+
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer(); //отрисовщик дебаг коллизии
+        game = gsm.game();
+        multiplexer = new InputMultiplexer();
+        cl = new MyContactListener(this); //детектит коллизию
+        world.setContactListener(cl);
+        music = Gdx.audio.newMusic(Gdx.files.internal("music/song.wav"));
+//        prefs = Gdx.app.getPreferences(PREF_NAME);
+//        savePlay = game.save;
+        skin_this = game.getSkin();
+
+        //initUI();
+        initJoyStick();
+        initController();
+        createPlayer();
+        createTiles();
+        createNPC();
+        //createMusic(); //отключено, чтобы не мешало при дебаггинге
+
+        //initFight();
+
+        cam.setBounds(0, tileMapWidth * tileSize * 4, 0, tileMapHeight * tileSize * 4);
+        b2dCam = new BoundedCamera(); //рисует дебаг коллизию?
+        b2dCam.setToOrtho(false, V_WIDTH / PPM, V_HEIGHT / PPM); // /2?
+        b2dCam.setBounds(0, (tileMapWidth * tileSize) / PPM, 0, (tileMapHeight * tileSize) / PPM);
+        System.out.println("V_HEIGHT: " + MyGdxGame.V_HEIGHT + " player: " + player.getPosition().x + " " + player.getPosition().y);
+    }
+
+    private void initController() {
+        controllerStage = new Stage(new ScreenViewport());
+        controllerStage.getViewport().update(V_WIDTH, V_HEIGHT, true);
+
+        controller = new Controller(skin_this);
+        controller.setVisible(true);
+
+        Table controllerRoot = new Table();
+        controllerRoot.setFillParent(true);
+        controllerRoot.add(controller).expand().align(Align.bottomLeft);
+        controllerStage.addActor(controllerRoot);
+
+        multiplexer.addProcessor(controllerStage);
+        Gdx.input.setInputProcessor(multiplexer);
+    }
+
+    private void initJoyStick() {
+        joyCam = new BoundedCamera();
+        joyCam.setBounds(0, V_WIDTH, 0, V_HEIGHT);
+        joyCam.setToOrtho(false, (float) (V_WIDTH), (float) (V_HEIGHT)); //не хватало этой строчки
+
+        joyStick = new JoyStick(200, 200, 200);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+        mouse = new Vector3();
+    }
+
+    @Override
+    public void handleInput() {
+    }
+
+    private void createPlayer() {
+        bdef = new BodyDef();
+        PolygonShape ps = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+
+        bdef.position.set(607f / PPM, 337f / PPM);
+
+
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        Body body = world.createBody(bdef);
+
+        ps.setAsBox(40f / PPM, 50f / PPM, new Vector2(-5.4f, -3.6f), 0);
+        fdef.shape = ps;
+        fdef.filter.categoryBits = BIT_PLAYER;
+        fdef.filter.maskBits = BIT_TROPA;
+        body.createFixture(fdef).setUserData("player");
+        ps.dispose();
+
+        //create foot sensor - дополнительная коллизия внизу игрока
+        /*ps.setAsBox(10f / PPM, 10f / PPM, new Vector2(0, -50f/PPM), 0);
+        fdef.shape = ps;
+        fdef.filter.categoryBits = BIT_PLAYER;
+        fdef.filter.maskBits = BIT_BLOCK;
+        fdef.isSensor = true;
+        body.createFixture(fdef).setUserData("foot");*/
+
+        player = new Player2(body);
+        player.setState(this);
+        body.setUserData(player);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl20.glClearColor(0, 0, 0, 1);
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        cam.setPosition(player.getPosition().x * PPM + V_WIDTH / 35, player.getPosition().y * PPM + V_HEIGHT / 35);
+        //cam.position.set(player.getPosition().x * PPM / 2, player.getPosition().y * PPM / 2, 0);
+        cam.update();
+
+        //draw map
+        tmr.setView(cam);
+        tmr.render();
+
+        //draw player and npc
+        sb.setProjectionMatrix(cam.combined); //https://stackoverflow.com/questions/33703663/understanding-the-libgdx-projection-matrix - объяснение
+        player.render(sb, 80f, 86.6f);
+        //boss.render(sb, 200f, 200f);
+        entities.render(sb, 150f, 150f);
+
+        //draw collision
+        if (debug) {
+            b2dCam.position.set(player.getPosition().x, player.getPosition().y, 0);
+            b2dCam.update();
+            b2dr.render(world, b2dCam.combined);
+        }
+
+        //draw initFight() if battle begin
+        if (canDraw) {
+            uiStage.draw();
+        }
+
+        controllerStage.draw();
+        joyStick.render(shapeRenderer);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+        world.step(dt, 6, 2);
+        controllerStage.act(dt);
+        player.update(dt);
+        //boss.update(dt);
+        entities.update(dt);
+        player.updatePL();
+
+        //нужно обновление размера экрана, и тогда будет resize всех компонентов?
+
+        //если этот state был выгружен, то при запуске все процессы должны возобновиться (удаляются ли они в multiplexer при выгрузке или просто останавливаются?)
+        if (isStopped) {
+            isStopped = false;
+            multiplexer.addProcessor(controllerStage);
+            Gdx.input.setInputProcessor(multiplexer);
+        }
+
+        if (controller.isMenuPressed()) {
+            gsm.setState(MENU);
+        }
+
+        //можно начать бой
+        if (canDraw) {
+            uiStage.act(dt);
+            dcontroller.update(dt);
+            time += dt;
+            if (dialogueBox.isFinished() && time > 2f) {
+                time = 0;
+                stop();
+            }
+        }
+    }
+
+    private void createTiles() {
+        tiledMap = new TmxMapLoader().load("sprites/mystic_woods_free_2.1/map.tmx");
+        tmr = new OrthogonalTiledMapRenderer(tiledMap, 4); // !!! размер карты
+        tileSize = (int) tiledMap.getProperties().get("tilewidth");
+
+        tileMapWidth = (int) tiledMap.getProperties().get("width");
+        tileMapHeight = (int) tiledMap.getProperties().get("height");
+
+        //TiledMapLayer walls = ...
+        //createLayer(walls, BIT_TROPA);
+    }
+
+    private void createNPC() {
+        MapLayer mlayer = tiledMap.getLayers().get("objects");
+        if (mlayer == null) return;
+        entities = new PlayEntities();
+
+        for (MapObject mo : mlayer.getObjects()) {
+            BodyDef bdef = new BodyDef();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            float x = (float) mo.getProperties().get("x") / PPM * 4;
+            float y = (float) mo.getProperties().get("y") / PPM * 4;
+            bdef.position.set(x, y);
+
+            Body body = world.createBody(bdef);
+            FixtureDef cdef = new FixtureDef();
+            CircleShape cshape = new CircleShape();
+            cshape.setRadius(50f / PPM);
+            cdef.shape = cshape;
+            cdef.isSensor = true;
+            cdef.filter.categoryBits = BIT_TROPA;
+            cdef.filter.maskBits = BIT_PLAYER;
+            cshape.dispose();
+
+            body.createFixture(cdef).setUserData(mo.getName());
+            entities.addEntity(body, mo.getName());
+
+        }
+    }
+
+    private void createLayer(TiledMapTileLayer layer, short bits) {
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+
+        for (int row = 0; row < layer.getHeight(); row++) {
+            for (int col = 0; col < layer.getWidth(); col++) {
+                TiledMapTileLayer.Cell cell = layer.getCell(col, row);
+                if (cell == null) {
+                    continue;
+                }
+                if (cell.getTile() == null) {
+                    continue;
+                }
+
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set(
+                        (col + 0.2f) * tileSize / 2.5f,
+                        (row + 0.4f) * tileSize / 2.5f);
+                ChainShape cs = new ChainShape();
+                Vector2[] v = new Vector2[3];
+                v[0] = new Vector2(-tileSize / 6, -tileSize / 6);
+                v[1] = new Vector2(-tileSize / 6, tileSize / 6);
+                v[2] = new Vector2(tileSize / 6, tileSize / 6);
+                cs.createChain(v);
+                fdef.friction = 0;
+                fdef.shape = cs;
+                fdef.filter.categoryBits = BIT_TROPA;
+                fdef.filter.maskBits = BIT_PLAYER;
+                fdef.isSensor = false;
+                world.createBody(bdef).createFixture(fdef).setUserData(layer.getName());
+                cs.dispose();
+            }
+        }
+    }
+
+    @Override
+    public JoyStick getJoyStick() {
+        return joyStick;
+    }
+
+    @Override
+    public void loadStage(String s) {
+        DialogNode node1;
+        initFight();
+        switch (s) {
+            case "enemy":
+                node1 = new DialogNode("Враг атакует!", 0);
+                dialog.addNode(node1);
+                dcontroller.startDialog(dialog);
+                nextState = BATTLE;
+                canDraw = true;
+                break;
+            case "npc":
+                node1 = new DialogNode("Начнем испытание!", 0);
+                dialog.addNode(node1);
+                dcontroller.startDialog(dialog);
+                nextState = PAINT;
+                canDraw = true;
+                break;
+            case "hooded":
+                node1 = new DialogNode("Идем в руины.", 0);
+                dialog.addNode(node1);
+                dcontroller.startDialog(dialog);
+                nextState = MAZE;
+                canDraw = true;
+                break;
+            case "next":
+                stop();
+                gsm.setState(FOREST);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initFight() {
+
+    }
+}
+
