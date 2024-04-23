336825ecf25e053596147eb7ca32216caffe3f29
sagaw <eta19777@gmail.com>
2024-04-23 15:56:06 +0300

    inventory

--- core/src/com/mygdx/game/handlers/GameStateManager.java
+++ core/src/com/mygdx/game/handlers/GameStateManager.java
@@ -23,7 +23,7 @@ public class GameStateManager {
         this.game = game;
         gameStates = new Stack<GameState>();
         //play = new Play(this);
-        pushState(NEW_GAME);
+        pushState(MENU);
     }
 
     //peek - get верхний элемент
